946a65f01b4d41cb83605aa6f61cfee3182e2da6
amdhousni <amed.housni@gmail.com>
2024-08-06 15:15:47 +0100

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,40 @@
 import streamlit as st
 import pandas as pd
 
-# Charger les données du film
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+# Read the CSV file into a DataFrame
 df_movies = pd.read_csv('movies.csv')
 
-# Définir le titre de l'application
-st.title('Système de Recommandation de Films')
+# Set the title of the app
+st.title('Movie Recommender System')
 
-# Créer une barre latérale pour les filtres
-st.sidebar.header('Filtres')
+# Add a text input field in the sidebar
+movie_title = st.sidebar.text_input("Titre du film")
 
-# Filtre par langue
-selected_languages = st.sidebar.multiselect('Langue', df_movies['language'].unique())
-filtered_movies = df_movies[df_movies['language'].isin(selected_languages)]
+# Filter the dataframe to get the row where the `title` column matches the `movie_title`
+selected_movie = df_movies[df_movies['title'] == movie_title]
 
-# Filtre par pays
-selected_countries = st.sidebar.multiselect('Pays', df_movies['country'].unique())
-filtered_movies = filtered_movies[filtered_movies['country'].isin(selected_countries)]
+# Extract the genres from the selected movie
+if not selected_movie.empty:
+    movie_genres = selected_movie['genres'].iloc[0].split(',')
 
+    # Create a new column `genre_match`
+    df_movies['genre_match'] = df_movies['genres'].astype(str).str.contains('|'.join(movie_genres), case=False)
 
-# Bouton pour afficher les films recommandés
-if st.sidebar.button('Afficher les recommandations'):
-    # Compter les occurrences de chaque film dans les données filtrées
-    movie_counts = filtered_movies['title'].value_counts()
+    # Filter the dataframe to keep only the rows where `genre_match` is True
+    recommended_movies = df_movies[df_movies['genre_match']]
 
-    # Trier les films par popularité décroissante et obtenir les 10 premiers
-    top_movies = movie_counts.head(10)
+    if not recommended_movies.empty:
+        # Display the genres of the selected movie
+        st.write(f"Genres du film sélectionné : {', '.join(movie_genres)}")
 
-    # Afficher les 10 films les plus populaires
-    st.header('Films recommandés')
-    st.write(top_movies.to_markdown(numalign="left", stralign="left"))
+        # Sample 10 movies from the recommended movies and display them
+        st.write(recommended_movies.sample(10)[['title', 'genres', 'language', 'country']].to_markdown(index=False, numalign="left", stralign="left"))
+
+    else:
+        st.write("Aucun film recommandé trouvé pour ces genres.")
+
+else:
+    st.write("Veuillez entrer un titre de film valide.")
