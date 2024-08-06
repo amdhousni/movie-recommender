86a8137fec527358a83d954ee5fa187f3d5c82e0
amdhousni <amed.housni@gmail.com>
2024-08-06 14:26:53 +0100

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ df_movies = pd.read_csv('movies.csv')
 # Set the title of the app
 st.title('Movie Recommender System')
 
+# Count the occurrences of each movie
+movie_counts = df_movies['title'].value_counts()
+
+# Sort the movie counts in descending order and get the top 10
+top_movies = movie_counts.head(10)
+
 # Display the top 10 most popular movies
 st.header('Most Popular Movies')
-top_movies = df_movies.sort_values(by='popularity', ascending=False).head(10)
-st.write(top_movies[['primaryTitle', 'genres', 'popularity', 'tmdbRating']].to_markdown(index=False, numalign="left", stralign="left"))
+st.write(top_movies.to_markdown(numalign="left", stralign="left"))
