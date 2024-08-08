1dbfe929c1d3a714fe252f0fdb960fdbecbd3225
amdhousni <amed.housni@gmail.com>
2024-08-08 22:50:02 +0100

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,24 @@ cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
 # Fonction pour obtenir des recommandations
 def get_recommendations(title, cosine_sim=cosine_sim):
-    # ... (logique pour trouver des films similaires en utilisant cosine_sim)
+    # Find the index of the movie that matches the title
+    idx = df[df['primaryTitle'] == title].index[0]
+
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return df['primaryTitle'].iloc[movie_indices]
+
 
 # Interface Streamlit
 st.title('Movie Recommender')
