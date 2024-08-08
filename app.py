66f0dca5e77cb2f275c37a268ced5029072cc0ae
amdhousni <amed.housni@gmail.com>
2024-08-08 22:58:05 +0100

    app.py

--- app.py
+++ app.py
@@ -19,24 +19,25 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     try:
         # Find the index of the movie that matches the title
         idx = df[df['primaryTitle'] == title].index[0]
+        
+        # Get the pairwise similarity scores of all movies with that movie
+        sim_scores = list(enumerate(cosine_sim[idx]))
+
+        # Sort the movies based on the similarity scores
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        # Get the scores of the 10 most similar movies
+        sim_scores = sim_scores[1:11]
+
+        # Get the movie indices
+        movie_indices = [i[0] for i in sim_scores]
+
+        # Return the top 10 most similar movies
+        return df['primaryTitle'].iloc[movie_indices]
+
     except IndexError:
         st.write("Movie not found in the dataset.")
         return []
-
-    # Get the pairwise similarity scores of all movies with that movie
-    sim_scores = list(enumerate(cosine_sim[idx]))
-
-    # Sort the movies based on the similarity scores
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    # Get the scores of the 10 most similar movies
-    sim_scores = sim_scores[1:11]
-
-    # Get the movie indices
-    movie_indices = [i[0] for i in sim_scores]
-
-    # Return the top 10 most similar movies
-    return df['primaryTitle'].iloc[movie_indices]
 
 # Streamlit interface
 st.title('Movie Recommender')
